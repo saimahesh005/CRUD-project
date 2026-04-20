a55530cb2a7b659afd40be13b0cde3dc6800e88b
Mahesh <saimahesh.b427@gmail.com>
2026-04-20 21:01:23 +0530

    updated views.py file

--- members/views.py
+++ members/views.py
@@ -1,71 +1,73 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.template import loader
 from .models import Employee
 
-# using loader and HttpResponse functions.
+
+# Display all employees
 def members(request):
-    template = loader.get_template('myfirsthtml.html')
-    myemployees = Employee.objects.all().values()
-    context = { 'myemployees': myemployees }
-    return HttpResponse(template.render(context, request))
+    myemployees = Employee.objects.all()
+    return render(request, 'myfirsthtml.html', {'myemployees': myemployees})
 
 
-
+# Home page
 def home(request):
-	template=loader.get_template('index.html')
-	return HttpResponse( template.render() )
+    return render(request, 'index.html')
 
+
+# ADD RECORD (POST)
 def addrecords(request):
-    fn=request.GET.get('text1')
-    ln=request.GET.get('text2')
-    email=request.GET.get('text3')
-    phone=request.GET.get('text4')
-    jobrole=request.GET.get('text5')
-    salary=request.GET.get('text6')
-    if fn and ln: 
-        employee1 = Employee(
-            firstname=fn,
-            lastname=ln,
-            email=email,
-            phone=phone,
-            jobrole=jobrole,
-            salary=salary
-        )
-        employee1.save()
+    if request.method == "POST":
+        fn = request.POST.get('text1')
+        ln = request.POST.get('text2')
+        email = request.POST.get('text3')
+        phone = request.POST.get('text4')
+        jobrole = request.POST.get('text5')
+        salary = request.POST.get('text6')
+
+        if fn and ln:
+            Employee.objects.create(
+                firstname=fn,
+                lastname=ln,
+                email=email,
+                phone=phone,
+                jobrole=jobrole,
+                salary=salary
+            )
         return redirect('members')
 
-# using render function instead of loader and HttpResponse.
+
+# UPDATE RECORD (POST)
 def updaterecords(request):
-    id = request.GET.get('id')
-    
-    try:
-        x = Employee.objects.get(id=id)
+    if request.method == "POST":
+        id = request.POST.get('id')
 
-        x.firstname = request.GET.get('text1')
-        x.lastname = request.GET.get('text2')
-        x.email = request.GET.get('text3')
-        x.phone = request.GET.get('text4')
-        x.jobrole = request.GET.get('text5')
-        x.salary = request.GET.get('text6')
+        try:
+            x = Employee.objects.get(id=id)
 
-        x.save()
+            x.firstname = request.POST.get('text1')
+            x.lastname = request.POST.get('text2')
+            x.email = request.POST.get('text3')
+            x.phone = request.POST.get('text4')
+            x.jobrole = request.POST.get('text5')
+            x.salary = request.POST.get('text6')
 
-    except Employee.DoesNotExist:
-        return HttpResponse("Employee not found")
+            x.save()
 
-    myemployees1 = Employee.objects.all().values()
-    context = {'myemployees': myemployees1}
+        except Employee.DoesNotExist:
+            return HttpResponse("Employee not found")
 
-    return render(request, 'myfirsthtml.html', context)
-    
+        return redirect('members')
+
+
+# DELETE RECORD (POST)
 def deleterecords(request):
-    id=request.GET.get('id')
-    try:
-            x=Employee.objects.get(id=id)
+    if request.method == "POST":
+        id = request.POST.get('id')
+
+        try:
+            x = Employee.objects.get(id=id)
             x.delete()
-    except Employee.DoesNotExist:
-         return HttpResponse("Employee not found")
-    myemployees1=Employee.objects.all().values()
-    context={'myemployees':myemployees1 }
-    return render(request, 'myfirsthtml.html', context)   
+        except Employee.DoesNotExist:
+            return HttpResponse("Employee not found")
+
+        return redirect('members')
